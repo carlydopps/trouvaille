7198d6756700246e97cda2152125deebf2c5aaa0
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-12-12 22:17:08 -0600

    Add view of my trips

--- src/components/managers/TripManager.js
+++ src/components/managers/TripManager.js
@@ -7,6 +7,15 @@ export const getTrips = () => {
         .then(response => response.json())
 }
 
+export const getMyTrips = () => {
+    return fetch("http://localhost:8000/trips?status=created", {
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("auth_token")}`
+        }
+     })
+        .then(response => response.json())
+}
+
 export const createTrip = (trip) => {
     return fetch("http://localhost:8000/trips", {
         method: "POST",
@@ -44,6 +53,17 @@ export const addTripExperience = (tripExperience) => {
         body: JSON.stringify(tripExperience)
      })
         .then(response => response.json())
+}
+
+export const deleteTrip = (id) => {
+    return fetch(`http://localhost:8000/trips/${id}`, {
+        method: "DELETE",
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            "Authorization": `Token ${localStorage.getItem("auth_token")}`
+        }
+    })
 }
 
 export const deleteTripExperience = (id) => {
