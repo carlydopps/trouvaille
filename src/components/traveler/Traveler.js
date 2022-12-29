1b32427b5146e1b2479f6d3f9b06bc14cfc2996b
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-12-28 21:17:12 -0600

    Add ability to follow travelers

--- src/components/traveler/Traveler.js
+++ src/components/traveler/Traveler.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { createSubscription, deleteSubscription } from "../managers/SubscriptionManager"
 import { getTraveler } from "../managers/TravelerManager"
 
 export const Traveler = () => {
@@ -14,24 +15,46 @@ export const Traveler = () => {
 
     })
 
+    const renderTraveler = () => {
+        getTraveler(travelerId)
+            .then(data => {
+                const convertedTraveler = {
+                    id: data.id,
+                    fullName: data.full_name,
+                    username: data.username,
+                    bio: data.bio,
+                    profileImage: data.profile_image_url,
+                    subscribed: data.subscribed,
+                    myself: data.myself
+                }
+                setTraveler(convertedTraveler)
+            })
+    }
+
     useEffect(
         () => {
-            getTraveler(travelerId)
-                .then(data => {
-                    const convertedTraveler = {
-                        id: data.id,
-                        fullName: data.full_name,
-                        username: data.username,
-                        bio: data.bio,
-                        profileImage: data.profile_image_url
-                    }
-                    setTraveler(convertedTraveler)
-                })
+            renderTraveler()
         }, 
         []
     )
+
+    const subscribe = (travelerId) => {
+        const newSubscription = {travelerId: travelerId}
+        createSubscription(newSubscription).then(() => renderTraveler())
+    }
+
+    const unsubscribe = (travelerId) => {
+        deleteSubscription(travelerId).then(() => renderTraveler())
+    }
+
     return <section>
         <img src={traveler.profileImage} alt="Profile Image" className="profile-image"/>
+        {traveler.myself
+            ? ""
+            : traveler.subscribed
+                ? <button onClick={() => unsubscribe(traveler.id)}>Unfollow</button>
+                : <button onClick={() => subscribe(traveler.id)}>Follow</button>
+        }
         <h4>{traveler.fullName}</h4>
             <p>@{traveler.username}</p>
     </section>      
