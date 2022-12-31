700e8be7f2621fc6d17dd9ae9508fc06cedd04c6
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-12-30 23:26:25 -0600

    Add styling to landing, auth, and traveler pages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Trouvaille } from './Trouvaille';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
